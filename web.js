daf33521fdb46bf7349d18c6a9a4fbef5543760d
Ubuntu <ubuntu@ip-10-151-114-196.ec2.internal>
2013-07-13 22:54:46 +0000

    Change to Sync.

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buffer = fs.readFile('index.html');
+var buffer = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
